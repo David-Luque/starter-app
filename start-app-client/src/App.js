5c6a5a3fe99b6559977718bff2fcf8675e349478
David L <dvdlq99@gmail.com>
2021-04-30 20:54:56 +0200

    created signup component

--- start-app-client/src/App.js
+++ start-app-client/src/App.js
@@ -6,14 +6,25 @@ import ProjectList from './components/projects/ProjectList';
 import ProjectDetails from './components/projects/ProjectDetails';
 import Navbar from './components/navbar/Navbar';
 import TaskDetails from './components/tasks/TaskDetails';
+import Signup from './components/auth/Signup'
 
 
 class App extends Component {
+  
+  state = {
+    loggedInUser: null
+  };
+
+  getTheUser = (userObject)=>{
+    this.setState({ loggedInUser:  userObject});
+  };
+
   render(){
     return (
       <div className="App">
-        <Navbar />
+        <Navbar userInSession={this.state.loggedInUser} />
         <Switch>
+          <Route exact path="/signup" render={()=>{<Signup getTheUser={this.getTheUser} />}} />
           <Route exact path="/projects" component={ProjectList} />
           <Route exact path="/projects/:id" component={ProjectDetails} />
           <Route exact path="/projects/:projectId/tasks/:taskId" component={TaskDetails} />
---
+++ start-app-client/src/components/auth/Signup.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import AuthService from './auth-service';
+
+class Signup extends Component {
+
+    state={
+        username: "",
+        password: ""
+    };
+
+    service = new AuthService();
+
+    handleFormSubmit = (event)=>{
+        event.preventDefault();
+        const { username, password } = this.state;
+        this.service.signup(username, password)
+        .then(response => {
+            this.setState({
+                username: "",
+                password: ""
+            });
+            this.props.getUser(response);
+        })
+        .catch(err => console.log(err))
+    };
+
+    handleChange = (event)=>{
+        const { name, value } = event.target;
+        this.setState({ [name]: value});
+    };
+
+    render(){
+        return(
+            <div>
+                <form onSubmit={this.handleFormSubmit}>
+                    <label>Username: </label>
+                    <input type="text" name="username" value={this.state.username} onSubmit={(e)=>{this.handleChange(e)}} />
+                    <label>Password: </label>
+                    <input type="password" name="password" value={this.state.password} onSubmit={(e)=>{this.handleChange(e)}} />
+                    <button>Sign up</button>
+                </form>
+                <p>
+                    Already have an account?
+                    <Link to={"/login"}>Log in</Link>
+                </p>
+            </div>
+        );
+    };
+};
+
+export default Signup;
